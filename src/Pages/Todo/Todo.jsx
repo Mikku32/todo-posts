35e00e9b6c858aa7561423246ae3e09af597ad66
Mikdhad.k <mikdhadk32@gmail.com>
2024-06-20 16:01:16 +0530

    api fetching

--- src/Pages/Todo/Todo.jsx
+++ src/Pages/Todo/Todo.jsx
@@ -1,51 +1,49 @@
-import React, { useState } from 'react'
-import TodoCard from './Components/TodoCard'
+import React, { useEffect, useState } from "react";
+import TodoCard from "./Components/TodoCard";
 
 const Todo = () => {
 
-  
+  const [todoList, setTodoList] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() =>{
+   const getData = async () =>{
+   try {
+    const res= await fetch("https://jsonplaceholder.typicode.com/todos")
 
 
+    if(!res.ok){
+     throw new Error("Something went wrong")}
 
+
+    const data= await res.json()
+
+    setTodoList(data)
+   } catch (error) {
+   setIsError(true)
+    } finally{
+      setIsLoading(false)
+    }
+   }
+
+   getData()
+  },[]
+)
+if(isError){
+  return <p>Something went wrong</p>
+}
+if(isLoading){
+  return <p>Loading...</p>
+}
   return (
-
     <div className="flex flex-col gap-6 ">
-     { dummyTodo.map((todo) => (
-        <TodoCard key={todo.id} todo={todo}/>
+      {todoList.map((todo) => (
+        <TodoCard key={todo.id} todo={todo} />
       ))}
-
     </div>
+  );
+};
 
-  )}
-
-  
-
-export default Todo
-
-
-
-const dummyTodo = [
-  {
-    userId:1,
-    id: 1,
-    title: "Do something",
-    completed: false
-
-  },
-  {
-    userId:1,
-    id: 2,
-    title: "Study the course",
-    completed: true
-  },
-  {
-    userId:2,
-    id: 3,
-    title: "do Homeworks",
-    completed: false
-  }
-]
-
-
-
+export default Todo;
 
